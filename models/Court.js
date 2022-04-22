6ed7d5ca92194445f11eaabe72b378223e600196
Marco Lölkes <marcoloelkes@gmx.de>
2022-04-22 18:24:08 +0200

    add models

---
+++ models/Court.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+
+const courtSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    location: {
+        lat: {
+            type: Number,
+            required: true
+        },
+        long: {
+            type: Number,
+            required: true
+        }
+    },
+    details: {
+        accessibility: {
+            fulltime: Boolean,
+            opening: Number, 
+            closing: Number
+        },
+        surface: {
+            type: String,
+            enum: [
+                'concrete',
+                'rubber',
+                'other'
+            ]
+        },
+        basketType: {
+            type: String,
+            enum: [
+                'chain',
+                'rope',
+                'bullshit'
+            ]
+        },
+        lighting: Boolean,
+        numBaskets: Number,
+    },
+    description: String,
+    image: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Court = model("Court", courtSchema);
+
+module.exports = Court;
---
+++ models/Event.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const eventSchema = new Schema(
+  {
+    organizer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    players: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    court: {
+        type: Schema.Types.ObjectId,
+        ref: 'Court'
+    },
+    date: Date,
+    name: String,
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Event = model("Event", eventSchema);
+
+module.exports = Event;
