07351a71efc6871423803cfd8947f34cf98a4cde
arneeder <arne.eder@gmail.com>
2022-04-25 18:00:29 +0200

    events can be created, route still needs to be protected

--- routes/event.js
+++ routes/event.js
@@ -6,11 +6,28 @@ const Event = require('../models/Event')
 const User = require('../models/User')
 
 router.get('/event/create', (req, res, next) => {
-    res.render('event/create')
+    Court.find()
+        .then(allCourts => {
+            let selectTags = ''
+            res.render('event/create', {allCourts: allCourts})
+        })
+        .catch(err => { next(err) })
 });
 
-// router.post('/event/create', (req, res, next) => {
-//     //
-// });
+router.post('/event/create', (req, res, next) => {
+    const {name, description, date, court} = req.body
+    Event.create({
+        name,
+        description,
+        date,
+        court,
+        organizer: req.session.user._id
+    })
+    .then( createdEvent => {
+        console.log('createdEvent :', createdEvent)
+        res.redirect('/main')
+    })
+    .catch(err => { next(err) })
+});
 
 module.exports = router;
