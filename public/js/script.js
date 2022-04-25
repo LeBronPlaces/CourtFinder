28b0145f46d57a48d069d49f60eef062d692bb39
Marco Lölkes <marcoloelkes@gmx.de>
2022-04-25 19:09:14 +0200

    add index view

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("courtFinder JS imported successfully!");
-  },
-  false
+    "DOMContentLoaded",
+    () => {
+        console.log("courtFinder JS imported successfully!");
+    },
+    false
 );
 
 const bootstrap = require('bootstrap')
