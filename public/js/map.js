9ea1a301581529576d00179b21f701c89923c360
Tommy Heyn <tommyheyn@gmail.com>
2022-04-28 11:24:12 +0200

    clickable markers etc.

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,11 @@
+//const res = require("express/lib/response");
+
 const map = createMap();
 createMarkers();
 map.on('click', addMarker)
 let actualMarker = null;
 let lastMarker = null
+
 
 function createMap() {
     mapboxgl.accessToken = 'pk.eyJ1IjoidGhiaCIsImEiOiJjbDJhZGVvbTgwMmQ2M2RucmliNXIwaDZ0In0.RrDkM5Omdqkq1EM_FXPxaQ';
@@ -23,10 +26,13 @@ function createMarkers() {
         .then(response => {
             let locations = response.data.locations;
             locations.forEach(coord => {
-                new mapboxgl.Marker({
+                let marker = new mapboxgl.Marker({
                     color: 'rgb(255, 123, 0)',
                 }).setLngLat(coord)
                     .addTo(map)
+                marker.getElement().addEventListener('click', (e) => {
+                    showCourtDetails(marker, e);
+                })
             })
         })
         .catch(err => {
@@ -34,14 +40,34 @@ function createMarkers() {
         })
 }
 
+function showCourtDetails(marker, e) {
+    console.log("hello");
+    let lat = marker._lngLat.lat;
+    let long = marker._lngLat.lng;
+    axios.get(`/courtByLocation/${lat}/${long}`)
+    .then(response => {
+        console.log(response);
+        let court = response.data.court
+
+        document.getElementById('map-info').innerHTML = `<p>${court.name}</p><p>${court.description}</p>`;
+    })
+
+    e.stopPropagation();
+}
+
 function addMarker(event) {
+    console.log('event: ', event);
     createMarker(event);
     showCreateMarkerForm();
 }
 
+function showCreateMarkerForm() {
+    console.log('create marker form called')
+}
+
 function createMarker(event) {
     actualMarker = event.lngLat;
-    document.getElementById('long').value = actualMarker.lng 
+    document.getElementById('long').value = actualMarker.lng
     if (lastMarker !== null) {
         lastMarker.remove();
     }
@@ -56,14 +82,18 @@ function createMarker(event) {
         draggable: true
     }).setLngLat(event.lngLat)
         .addTo(map)
+    lastMarker.getElement().addEventListener('click', (e) => {
+        
+        console.log(marker);
+        showCourtDetails(marker, e);
+    })
+
     //console.log("lastMarker: ", lastMarker);
 }
 
 
-function showCreateMarkerForm() {
-    document.getElementById('').innerHTML = '<p></p>'
 
-}
+
 
 function toggleOpeningTimes() {
 
