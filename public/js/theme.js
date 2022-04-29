861c62319bf964bad3666d39cfc4134ccda8b3ac
Marco Lölkes <marcoloelkes@gmx.de>
2022-04-29 11:02:36 +0200

    add toggle for button text

--- public/js/theme.js
+++ public/js/theme.js
@@ -11,20 +11,35 @@ function setInitialDocumentCookie() {
 function setInitialStylesheet() {
     if (document.cookie == "la") {
         document.getElementById('stylesheet-file').innerHTML = "<link rel='stylesheet' href='/stylesheets/style.css' />";
+        document.getElementById('toggle-theme').innerText = 'LA';
     } else {
         document.getElementById('stylesheet-file').innerHTML = "<link rel='stylesheet' href='/stylesheets/style-dark.css' />";
+        document.getElementById('toggle-theme').innerText = 'B';
     }
 }
 
 function setClickHandlerForToggleButton() {
     document.getElementById('toggle-theme').addEventListener('click', () => {
-        console.log(document.cookie);
     if (document.cookie == "la") {
         document.getElementById('stylesheet-file').innerHTML = "<link rel='stylesheet' href='/stylesheets/style-dark.css' />";
         document.cookie = "berlin"
+        toggleButtonText();
     } else {
         document.getElementById('stylesheet-file').innerHTML = "<link rel='stylesheet' href='/stylesheets/style.css' />";
         document.cookie = "la"
+        toggleButtonText();
         }
     })
+}
+
+function toggleButtonText() {
+    let toggleButtonText = document.getElementById('toggle-theme').innerText;
+
+    if (toggleButtonText == 'B') {
+        document.getElementById('toggle-theme').innerText = 'LA';
+        console.log('LA');
+    } else {
+        document.getElementById('toggle-theme').innerText = 'B';
+        console.log('B')
+    }
 }
